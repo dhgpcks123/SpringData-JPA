f34accf6af5357acafc566aab013ff237a004eb2
hyechan <dhgpcks@gmail.com>
2021-06-04 22:44:27 +0900

    공통 인터페이스 설정 및 적용

--- src/main/java/study/datajpa/repository/TeamRepository.java
+++ src/main/java/study/datajpa/repository/TeamRepository.java
@@ -1,10 +1,10 @@
 package study.datajpa.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import study.datajpa.entity.Member;
+import study.datajpa.entity.Team;
 
 //@Repository //어노테이션 생략해도 됨!
 //왜냐면! 스프링 데이터 JPA가 인터페이스만 보고 인식해줌
-public interface MemberRepository extends JpaRepository<Member, Long> {
-    //여기 <Member, Long>에 Long은 pk 데이터타입 넣어주면 됨
+public interface TeamRepository extends JpaRepository<Team, Long> {
+    //여기 <Team, Long>에 Long은 pk 데이터타입 넣어주면 됨
 }
--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -9,6 +9,7 @@ import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 import study.datajpa.entity.Member;
 
+import java.util.List;
 import java.util.Optional;
 
 @SpringBootTest
@@ -21,6 +22,7 @@ public class MemberRepositoryTest {
     @Test
     @DisplayName("회원가입 테스트")
     public void testMember(){
+        System.out.println("memberRepository = " + memberRepository.getClass());
         Member member = new Member("memberA");
         Member savedMember = memberRepository.save(member);
 
@@ -31,4 +33,38 @@ public class MemberRepositoryTest {
         Assertions.assertThat(findMember).isEqualTo(member);
     }
 
+    @Test
+    @DisplayName("JpaRepository상속 동작")
+    public void basicCRUD(){
+        Member member1 = new Member("member1");
+        Member member2 = new Member("member2");
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+
+        Member findMember1 = memberRepository.findById(member1.getId()).get();
+        Member findMember2 = memberRepository.findById(member2.getId()).get();
+
+//        findMember1.setUsername("member!!!!!!");
+
+
+        //단건 조회 검증
+        Assertions.assertThat(findMember1).isEqualTo(member1);
+        Assertions.assertThat(findMember2).isEqualTo(member2);
+
+        //리스트 조회 검증
+        List<Member> all = memberRepository.findAll();
+        Assertions.assertThat(all.size()).isEqualTo(2);
+
+        //카운트 조회 검증
+        long count = memberRepository.count();
+        Assertions.assertThat(count).isEqualTo(2);
+
+        //삭제 검증
+        memberRepository.delete(member1);
+        memberRepository.delete(member2);
+
+        long deletedCount = memberRepository.count();
+        Assertions.assertThat(deletedCount).isEqualTo(0);
+
+    }
 }
